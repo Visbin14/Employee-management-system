c5bf2927b17877ced03e78cfd9cb80d3837de0e0
visbin14 <visbinrojer@codesvera.com>
2022-11-04 18:38:35 +0530

    first commit

--- Ems/views.py
+++ Ems/views.py
@@ -1,11 +1,89 @@
 from django.shortcuts import render
-
 from scrape import break_login_logout
-from skpy import *
 from Ems.models import *
+from skpy import *
+
 sk = Skype("visbinrojer@codesvera.com", "v@visbin@code@pass")
 
 def home(request):
+
+    log_time = break_login_logout()
+    def log():
+        for i in log_time:
+            
+            # lis = []
+            if i.content.lower() == "break":
+                # print("\n","break : ",i)
+                a = str(i)
+                b= a.split("UserId")[1].strip()[1:].strip().split("ChatId:")[0].strip()
+                date = a.split("Time")[1].split()[1:2]
+                time= str(a.split("Time")[1].split()[2:3])
+                time=time [2:10]
+                contact = sk.contacts[b]
+                name = contact.name
+                print("break:",name,date,time)
+                
+               
+            
+            # lis = []
+            if i.content.lower().replace(" ","") == "goodmorning":
+                # print("\n","login : ",i)
+                a = str(i)
+                b= a.split("UserId")[1].strip()[1:].strip().split("ChatId:")[0].strip()
+                date = a.split("Time")[1].split()[1:2]
+                time= str(a.split("Time")[1].split()[2:3])
+                time=time [2:10]
+                contact = sk.contacts[b]
+                name = contact.name
+                print("login:",name,date,time)
+                
+            
+            # lis = []
+            if i.content.lower().replace(" ","") == "backtowork":
+                # print("\n","back to work : ", i)
+                a = str(i)
+                b= a.split("UserId")[1].strip()[1:].strip().split("ChatId:")[0].strip()
+                date = a.split("Time")[1].split()[1:2]
+                time= str(a.split("Time")[1].split()[2:3])
+                time=time [2:10]
+                contact = sk.contacts[b]
+                name = contact.name
+                
+                print("back to work:",name,date,time)
+                
+               
+            
+            
+            # lis = []
+            if "what did you do today" in i.content.lower():
+                # print("\n","logout : ",i)
+                a = str(i)
+                b= a.split("UserId")[1].strip()[1:].strip().split("ChatId:")[0].strip()
+                date = a.split("Time")[1].split()[1:2]
+                time= str(a.split("Time")[1].split()[2:3])
+                time=time [2:10]
+                contact = sk.contacts[b]
+                name = str(contact.name)
+                print("logout:",name,date,time)
+                
+                        
+               
+       
+    
+    log()
+
+         
+    
+      
+    return render(request,"home.html")
+
+
+
+
+
+
+
+
 
     # # login
     
@@ -52,54 +130,6 @@ def home(request):
 
     #     print(name,date,time)
 
-
-
-    log_time = break_login_logout()
-    def break_time():
-        lis = []
-        for i in log_time:
-            lis.append(i)
-
-        
-            if i.content.lower() == "break":
-                print("\n",i)
-       
-    
-    def back_to_work():
-        lis = []
-        for i in log_time:
-            lis.append(i)
-
-        
-            if i.content.lower().replace(" ","") == "backtowork":
-                print("\n",i)
-       
-    
-
-    def login():
-        lis = []
-        for i in log_time:
-            lis.append(i)
-
-        
-            if i.content.lower().replace(" ","") == "goodmorning":
-                print("\n",i)
-    
-
-    def logout():
-        lis = []
-        for i in log_time:
-            lis.append(i)
-
-        
-            if "what did you do today" in i.content.lower():
-                print("\n",i)
-
-    break_time()
-    back_to_work()
-    login()    
-    logout()
-    return render(request,"home.html")
 
 
 
--- scrape.py
+++ scrape.py
@@ -1,6 +1,6 @@
 from skpy import *
 
-from Ems.models import Employee,Log_status,Logged_Time
+
 
 import os, django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Employee_Management_System.settings")
@@ -9,84 +9,114 @@ django.setup()
 sk = Skype("visbinrojer@codesvera.com", "v@visbin@code@pass")
 
 
-# Break
-"""
-def breaktime():
-
-    ch = sk.chats["19:1fe0d47033cd4ee692cbd378f28f50f5@thread.skype"]
-    c = ch.getMsgs()
-    h = ch.getMsgs()
-    # a = ch.getMsgs()
-    new_list = c+h
-
-    lis = []
-    for i in new_list:
-        # print("\n",i)
-        if i.content.lower() == "break":
-            lis.append(i)
-
-    for i in lis:
-        print(i)
-
-"""
-
-# login
-
-
-# def login():
-#     ch = sk.chats["19:1fe0d47033cd4ee692cbd378f28f50f5@thread.skype"]
-#     c = ch.getMsgs()
-#     h = ch.getMsgs()
-   
-
-#     new_list = c+h
-    
-
-#     lis = []
-#     for i in new_list:
-        
-#         if i.content.lower().replace(" ","") == "goodmorning":
-#             lis.append(i)
-
-#     return lis
-
-  
-    
-
-# # logout
-
-
-# def logout():
-#     ch = sk.chats["19:1fe0d47033cd4ee692cbd378f28f50f5@thread.skype"]
-#     c = ch.getMsgs()
-#     h = ch.getMsgs()
-#     new_list = c+h
-    
-
-#     lis = []
-#     for i in new_list:
-#         message= i.content
-#         if "what did you do today" in message.lower():
-#             lis.append(i)
-
-    
-#     return lis
-
-
-
-
-
 def break_login_logout():
     ch = sk.chats["19:1fe0d47033cd4ee692cbd378f28f50f5@thread.skype"]
     c = ch.getMsgs()
     h = ch.getMsgs()
-    new_list = c+h
+    a = ch.getMsgs()
+    t = ch.getMsgs()
+    old_list = c+h
+    new_list = a+t
+    combined_list = old_list+new_list
 
     lis = []
     
-    for i in new_list:
+    for i in combined_list:
         lis.append(i)
 
 
     return lis
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Break
+# """
+# def breaktime():
+
+#     ch = sk.chats["19:1fe0d47033cd4ee692cbd378f28f50f5@thread.skype"]
+#     c = ch.getMsgs()
+#     h = ch.getMsgs()
+#     # a = ch.getMsgs()
+#     new_list = c+h
+
+#     lis = []
+#     for i in new_list:
+#         # print("\n",i)
+#         if i.content.lower() == "break":
+#             lis.append(i)
+
+#     for i in lis:
+#         print(i)
+
+# """
+
+# # login
+
+
+# # def login():
+# #     ch = sk.chats["19:1fe0d47033cd4ee692cbd378f28f50f5@thread.skype"]
+# #     c = ch.getMsgs()
+# #     h = ch.getMsgs()
+   
+
+# #     new_list = c+h
+    
+
+# #     lis = []
+# #     for i in new_list:
+        
+# #         if i.content.lower().replace(" ","") == "goodmorning":
+# #             lis.append(i)
+
+# #     return lis
+
+  
+    
+
+# # # logout
+
+
+# # def logout():
+# #     ch = sk.chats["19:1fe0d47033cd4ee692cbd378f28f50f5@thread.skype"]
+# #     c = ch.getMsgs()
+# #     h = ch.getMsgs()
+# #     new_list = c+h
+    
+
+# #     lis = []
+# #     for i in new_list:
+# #         message= i.content
+# #         if "what did you do today" in message.lower():
+# #             lis.append(i)
+
+    
+# #     return lis
+
+
+
+
+
 
