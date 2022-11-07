fc0563d6d27c32f4e897a9f47754d0d91e2735fe
anjithacodesvera <anjitha.s@codesvera.com>
2022-11-07 15:05:35 +0530

    UI design, dashboard design and add employee

--- Employee_Management_System/settings.py
+++ Employee_Management_System/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Employee_Management_System.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,"Template/Ems")],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/log')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,6 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATIC_ROOT=os.path.join(BASE_DIR,'assets')
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- Ems/urls.py
+++ Ems/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from Ems import views
 
 urlpatterns = [
-    path("",views.home,name = "home")
-
+    path("",views.home,name = "home"),
+    path('login/', views.login, name='login'),
+    path('index/', views.index, name='index'),
+    path('employee/', views.employee, name='employee'),
+    path('add/', views.add, name='add'),
 ]
