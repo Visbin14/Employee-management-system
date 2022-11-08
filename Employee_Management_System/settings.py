36e970b89dbff00c43a1fd4a78bbedadeb1a8f0b
visbin14 <visbinrojer@codesvera.com>
2022-11-08 18:43:59 +0530

    env changes and chnages in view

--- Employee_Management_System/settings.py
+++ Employee_Management_System/settings.py
@@ -12,16 +12,17 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8c6#+q@%q(y1hbg=il*a##c#*^%4gyv!6mc6c4992ciq4l@vrt'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,9 +79,9 @@ WSGI_APPLICATION = 'Employee_Management_System.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pmtool',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
@@ -131,3 +132,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SKYPE_EMAIL = env('SK_EMAIL')
+SKYPE_PASS = env('SK_PASS')
--- Ems/views.py
+++ Ems/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from scrape import break_login_logout
 from Ems.models import *
+from Ems.Log import logged
 from skpy import *
 
 sk = Skype("visbinrojer@codesvera.com", "v@visbin@code@pass")
@@ -11,9 +12,8 @@ def home(request):
     def log():
         for i in log_time:
             
-            # lis = []
-            if i.content.lower() == "break":
-                # print("\n","break : ",i)
+            
+            if i.content.lower() == logged.get('Break'):
                 a = str(i)
                 b= a.split("UserId")[1].strip()[1:].strip().split("ChatId:")[0].strip()
                 date = a.split("Time")[1].split()[1:2]
@@ -21,25 +21,17 @@ def home(request):
                 time=time [2:10]
                 contact = sk.contacts[b]
                 name = contact.name
-                # print("break:",name,date,time)
                 obj = Employee.objects.get(name = name)
                 
-                if i.content.lower() == "break":
-                    log = Log_status.objects.create(Emp = obj, Log = "break", Time=time)
-                    log.save()
-                    logged_time = Logged_Time.objects.create(Employee=obj, Date=date[0])
-                    logged_time.Log.add(log)  
                
-
-              
-                
-
-
-             
+                log = Log_status.objects.create(Emp = obj, Log = "break", Time=time)
+                log.save()
+                    
+                logged_time = Logged_Time.objects.create(Employee=obj, Date=date[0])
+                logged_time.Log.add(log)  
             
-            # lis = []
-            if i.content.lower().replace(" ","") == "goodmorning":
-                # print("\n","login : ",i)
+            
+            if i.content.lower().replace(" ","") == logged.get('Login'):
                 a = str(i)
                 b= a.split("UserId")[1].strip()[1:].strip().split("ChatId:")[0].strip()
                 date = a.split("Time")[1].split()[1:2]
@@ -47,20 +39,17 @@ def home(request):
                 time=time [2:10]
                 contact = sk.contacts[b]
                 name = contact.name
-                
                 obj1 = Employee.objects.get(name = name)
                 
-                if i.content.lower().replace(" ","") == "goodmorning":
-                    log = Log_status.objects.create(Emp = obj1, Log = "login", Time=time)
-                    log.save()
+                
+                log = Log_status.objects.create(Emp = obj1, Log = "login", Time=time)
+                log.save()
                     
-                    logged_time = Logged_Time.objects.create(Employee=obj1, Date=date[0])
-                    logged_time.Log.add(log)
-                # print("login:",name,date,time)
+                logged_time = Logged_Time.objects.create(Employee=obj1, Date=date[0])
+                logged_time.Log.add(log)
                 
             
-            if i.content.lower().replace(" ","") == "backtowork":
-                # print("\n","back to work : ", i)
+            if i.content.lower().replace(" ","") == logged.get('Back_to_work'):
                 a = str(i)
                 b= a.split("UserId")[1].strip()[1:].strip().split("ChatId:")[0].strip()
                 date = a.split("Time")[1].split()[1:2]
@@ -70,20 +59,15 @@ def home(request):
                 name = contact.name
                 obj2 = Employee.objects.get(name = name)
                 
-                if i.content.lower().replace(" ","") == "backtowork":
-                    log = Log_status.objects.create(Emp = obj2, Log = "back to work", Time=time)
-                    log.save()
-                    logged_time = Logged_Time.objects.create(Employee=obj2, Date=date[0])
-                    logged_time.Log.add(log)
                 
-                # print("back to work:",name,date,time)
+                log = Log_status.objects.create(Emp = obj2, Log = "back to work", Time=time)
+                log.save()
+                    
+                logged_time = Logged_Time.objects.create(Employee=obj2, Date=date[0])
+                logged_time.Log.add(log)
                 
-               
-            
-            
-            # lis = []
-            if "what did you do today" in i.content.lower():
-                # print("\n","logout : ",i)
+              
+            if logged.get('Logout') in i.content.lower():
                 a = str(i)
                 b= a.split("UserId")[1].strip()[1:].strip().split("ChatId:")[0].strip()
                 date = a.split("Time")[1].split()[1:2]
@@ -93,22 +77,17 @@ def home(request):
                 name = contact.name
                 obj3 = Employee.objects.get(name = name)
                 
-                if "what did you do today" in i.content.lower():
-                    log = Log_status.objects.create(Emp = obj3, Log = "logout", Time=time)
-                    log.save()
-                    logged_time = Logged_Time.objects.create(Employee=obj3, Date=date[0])
-                    logged_time.Log.add(log)
-                # print("logout:",name,date,time)
                 
-                        
-               
-       
+                log = Log_status.objects.create(Emp = obj3, Log = "logout", Time=time)
+                log.save()
+                    
+                logged_time = Logged_Time.objects.create(Employee=obj3, Date=date[0])
+                logged_time.Log.add(log)
+                      
     
     log()
 
-         
-    
-      
+           
     return render(request,"home.html")
 
 
@@ -116,53 +95,6 @@ def home(request):
 
 
 
-
-
-
-    # # login
-    
-    # Login= login()
-    # for i in Login:
-      
-    #     a= str(i)
-    #     b= a.split("UserId")[1].strip()[1:].strip().split("ChatId:")[0].strip()
-
-       
-
-    #     date = a.split("Time")[1].split()[1:2]
-
-    #     time= str(a.split("Time")[1].split()[2:3])
-    #     time=time [2:10]
-        
-    #     contact = sk.contacts[b]
-
-       
-        
-    #     name = contact.name
-
-    #     print(name, date, time)
-        
-    #     # break
-       
- 
-    # print("-----------------")
-
-    # # logout
-
-    # Logout = logout()
-    # for i in Logout:
-    #     a = str(i)
-    #     b= a.split("UserId")[1].strip()[1:].strip().split("ChatId:")[0].strip()
-    #     date = a.split("Time")[1].split()[1:2]
-
-    #     time= str(a.split("Time")[1].split()[2:3])
-    #     time=time [2:10]
-        
-    #     contact = sk.contacts[b]
-    #     name = contact.name
-
-
-    #     print(name,date,time)
 
 
 
